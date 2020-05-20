74ba1afaac22e52c68ee0eda755a3467acbffc97
cyanray <a484599279@hotmail.com>
2020-05-20 18:02:17 +0800

    登录示例;

---
+++ examples/Login.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <qzjw.h>
+
+int main()
+{
+#if defined(_WIN32) || defined(WIN32)
+	system("chcp 65001");
+#endif
+	using namespace std;
+	using namespace cyanray;
+
+	Jw jw("http://jwgl.cqjtu.edu.cn/");
+
+	string uid, pwd;
+	cout << "请输入用户名和密码: " << endl;
+	cin >> uid >> pwd;
+	try
+	{
+		jw.Login(uid, pwd);
+		cout << "登录成功, token: " << jw.GetToken() << endl;
+	}
+	catch (const exception& ex)
+	{
+		cout << "登录出错: " << ex.what() << endl;
+	}
+	
+	
+	return 0;
+}
